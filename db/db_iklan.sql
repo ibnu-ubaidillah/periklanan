8eebd4f7e8be543db20057ffeeb3c2bf5ee4f82f
Ibnu Ubaidillah <ibnu048@gmail.com>
2020-08-20 21:47:04 +0700

    tambah db

--- db/db_iklan.sql
+++ db/db_iklan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 18, 2020 at 08:23 AM
+-- Generation Time: Aug 20, 2020 at 04:46 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
@@ -53,6 +53,20 @@ INSERT INTO `tbl_detailpaket` (`id_detail`, `id_paket`, `id_tipepaket`, `jumlah_
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_jadwal`
+--
+
+CREATE TABLE `tbl_jadwal` (
+  `id_jadwal` int(11) NOT NULL,
+  `id_pembayaran` int(11) NOT NULL,
+  `id_pengajuan` int(11) NOT NULL,
+  `tanggal` int(11) NOT NULL,
+  `jam` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_paket`
 --
 
@@ -73,6 +87,33 @@ INSERT INTO `tbl_paket` (`id_paket`, `nama_paket`, `tgl_dibuat`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_pembayaran`
+--
+
+CREATE TABLE `tbl_pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_pengajuan` int(11) NOT NULL,
+  `id_pengguna` int(11) NOT NULL,
+  `id_rekening` int(11) NOT NULL,
+  `kode_pembayaran` varchar(25) NOT NULL,
+  `invoice` varchar(128) NOT NULL DEFAULT '-',
+  `bukti_pembayaran` varchar(255) NOT NULL DEFAULT '-',
+  `tanggal` int(11) NOT NULL,
+  `status_p` varchar(20) NOT NULL DEFAULT 'Belum Lunas',
+  `tgl_terakhir` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pembayaran`
+--
+
+INSERT INTO `tbl_pembayaran` (`id_pembayaran`, `id_pengajuan`, `id_pengguna`, `id_rekening`, `kode_pembayaran`, `invoice`, `bukti_pembayaran`, `tanggal`, `status_p`, `tgl_terakhir`) VALUES
+(2, 4, 1, 2, 'KB0001', '', '', 20200820, 'Belum Lunas', '2020-08-20 10:41:16'),
+(3, 6, 2, 1, 'KB0002', 'AC2008200001', 'bukti-20082020-b242d74239.jpg', 20200820, 'Lunas', '2020-08-20 10:58:49');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_pengajuan`
 --
 
@@ -80,21 +121,26 @@ CREATE TABLE `tbl_pengajuan` (
   `id_pengajuan` int(11) NOT NULL,
   `kode_pengajuan` varchar(25) NOT NULL,
   `id_pengguna` int(11) NOT NULL,
-  `id_paket` int(11) NOT NULL,
   `id_detail` int(11) NOT NULL,
   `konten` varchar(255) NOT NULL,
   `caption` varchar(255) NOT NULL,
   `tanggal` int(11) NOT NULL,
   `status` varchar(25) NOT NULL DEFAULT 'Pending',
-  `keterangan` varchar(25) NOT NULL DEFAULT '-'
+  `keterangan` varchar(255) NOT NULL DEFAULT '-',
+  `update_pada` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tbl_pengajuan`
 --
 
-INSERT INTO `tbl_pengajuan` (`id_pengajuan`, `kode_pengajuan`, `id_pengguna`, `id_paket`, `id_detail`, `konten`, `caption`, `tanggal`, `status`, `keterangan`) VALUES
-(1, 'KP0001', 6, 2, 1, 'IMAGE.jpg', 'Ini adalah caption', 20200817, 'Pending', '-');
+INSERT INTO `tbl_pengajuan` (`id_pengajuan`, `kode_pengajuan`, `id_pengguna`, `id_detail`, `konten`, `caption`, `tanggal`, `status`, `keterangan`, `update_pada`) VALUES
+(1, 'KP0001', 6, 1, 'IMAGE.jpg', 'Ini adalah caption', 20200817, 'Diterima', 'Bagus sekaliii', '2020-08-19 17:09:34'),
+(2, 'KP0002', 1, 6, 'konten-19082020-655d2bde15.jpg', '6', 19, 'Ditolak', 'Iklan kamu mengandung sar', '2020-08-19 17:09:34'),
+(3, 'KP0003', 1, 3, 'konten-19082020-78653ab31d.jpg', '3', 19082020, 'Pending', '-', '2020-08-19 17:09:34'),
+(4, 'KP0004', 1, 3, 'konten-19082020-24257456c1.jpg', ' asddgdsghsgs', 20200819, 'Diterima', 'Ini sangat bagus sekalii', '2020-08-19 17:09:34'),
+(5, 'KP0005', 2, 5, 'konten-19082020-290a2863cc.jpg', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus suscipit saepe eum eaque consequatur autem in pariatur praesentium. Distinctio rem illum excepturi laborum dicta molestiae? Explicabo eaque ullam omnis.', 20200819, 'Ditolak', 'Iklan kamu mengandung Provokasiii ! silahkan ganti', '2020-08-19 17:09:34'),
+(6, 'KP0006', 2, 7, 'konten-20082020-6218ad4aa1.jpg', ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim ab deleniti alias id, magni impedit praesentium placeat aut dolorem voluptatem exercitationem, quod incidunt pariatur possimus quidem! Ipsam illum soluta quis!\r\n    }', 20200820, 'Diterima', 'Sangat bagus sekali', '2020-08-20 07:05:11');
 
 -- --------------------------------------------------------
 
@@ -127,6 +173,27 @@ INSERT INTO `tbl_pengguna` (`id_pengguna`, `username`, `password`, `email`, `nam
 (3, 'Khairun', '585b2f7c7b45e93a7934ba3a95e059413a08d06a', 'nnas54@gmail.com', 'Khairunnas', 'Cirebon', '083234278654', 'Laki-laki', 2, '2020-08-10 10:11:35', 1, '2020-08-10 10:11:35'),
 (4, 'Dandi', '6467baa3b187373e3931422e2a8ef22f3e447d77', 'Dandi_@hotmail.com', 'Dandi Agustian Syah', '   Surabaya', '089245345678', 'Laki-laki', 3, '2020-08-10 10:11:39', 1, '2020-08-10 10:11:39'),
 (6, 'Faisal', 'a6addb9ffb18bd870b260590177a30e43a3d218d', 'Zafkhiell444@yahoo.co.id', 'Akhmad Faisall', 'Cirebon', '089234211245', 'Laki-laki', 3, '2020-08-10 09:34:14', 1, '2020-08-10 09:34:14');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_rekening`
+--
+
+CREATE TABLE `tbl_rekening` (
+  `id_rekening` int(11) NOT NULL,
+  `nama_bank` varchar(50) NOT NULL,
+  `no_rekening` varchar(50) NOT NULL,
+  `atas_nama` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_rekening`
+--
+
+INSERT INTO `tbl_rekening` (`id_rekening`, `nama_bank`, `no_rekening`, `atas_nama`) VALUES
+(1, 'BCA', '3740250824', 'Agus Sapiyullah'),
+(2, 'Mandiri', '1340005695951', 'Agus Sapiyullah');
 
 -- --------------------------------------------------------
 
@@ -166,6 +233,12 @@ ALTER TABLE `tbl_paket`
   ADD PRIMARY KEY (`id_paket`);
 
 --
+-- Indexes for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
 -- Indexes for table `tbl_pengajuan`
 --
 ALTER TABLE `tbl_pengajuan`
@@ -176,6 +249,12 @@ ALTER TABLE `tbl_pengajuan`
 --
 ALTER TABLE `tbl_pengguna`
   ADD PRIMARY KEY (`id_pengguna`);
+
+--
+-- Indexes for table `tbl_rekening`
+--
+ALTER TABLE `tbl_rekening`
+  ADD PRIMARY KEY (`id_rekening`);
 
 --
 -- Indexes for table `tbl_tipepaket`
@@ -200,10 +279,16 @@ ALTER TABLE `tbl_paket`
   MODIFY `id_paket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
+-- AUTO_INCREMENT for table `tbl_pembayaran`
+--
+ALTER TABLE `tbl_pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `tbl_pengajuan`
 --
 ALTER TABLE `tbl_pengajuan`
-  MODIFY `id_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_pengajuan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_pengguna`
@@ -212,10 +297,16 @@ ALTER TABLE `tbl_pengguna`
   MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
+-- AUTO_INCREMENT for table `tbl_rekening`
+--
+ALTER TABLE `tbl_rekening`
+  MODIFY `id_rekening` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `tbl_tipepaket`
 --
 ALTER TABLE `tbl_tipepaket`
-  MODIFY `id_tipepaket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_tipepaket` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
